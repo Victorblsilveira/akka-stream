3691a5620277e52332955afc3cb7cacfedbe8c2d
Victor Vieira Barros Leal <victorblsilveira@gmail.com>
2020-07-09 17:30:13 -0300

    Finalizando Aula de ciclos em grafos

--- src/main/scala/part3_graphs/GraphCycles.scala
+++ src/main/scala/part3_graphs/GraphCycles.scala
@@ -1,5 +1,129 @@
 package part3_graphs
 
-object GraphCycles {
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, ClosedShape, FanInShape, OverflowStrategy, UniformFanInShape}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, MergePreferred, RunnableGraph, Sink, Source, Zip, ZipWith}
+
+object GraphCycles extends App {
+
+  implicit val system = ActorSystem("GraphCycles")
+  implicit val materializer = ActorMaterializer()
+
+  val accelerator = GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
+
+    val sourceShape = builder.add(Source(1 to 100))
+    val mergeShape = builder.add(Merge[Int](2))
+    val incrementerShape = builder.add(Flow[Int].map { x =>
+      println(s"Accelerating $x")
+      x + 1
+    })
+
+    sourceShape ~> mergeShape ~> incrementerShape
+                   mergeShape <~ incrementerShape
+    ClosedShape
+  }
+
+  // RunnableGraph.fromGraph(accelerator).run()
+  // Graph cycle deadlock!!!! Just 1 element has been emited because of the backpressure.
+
+  /*
+    Solution 1:  MergePreferred -
+     - Elements on the preferred port are used 'preferred' than the others ports
+  */
+
+  val actualAccelerator = GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
+
+    val sourceShape = builder.add(Source(1 to 100))
+    val mergeShape = builder.add(MergePreferred[Int](1))
+    val incrementerShape = builder.add(Flow[Int].map { x =>
+      println(s"Accelerating $x")
+      x + 1
+    })
+
+    sourceShape ~> mergeShape ~> incrementerShape
+    mergeShape.preferred <~ incrementerShape
+    ClosedShape
+  }
+
+ // RunnableGraph.fromGraph(actualAccelerator).run()
+
+  /*
+  Solution 2:  Buffers -
+   - Elements on the preferred port are used 'preferred' than the others ports
+*/
+  val bufferRepeater = GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
+
+    val sourceShape = builder.add(Source(1 to 100))
+    val mergeShape = builder.add(Merge[Int](2))
+    val repeaterShape = builder.add(Flow[Int].buffer(10, OverflowStrategy.dropHead).map { x =>
+      println(s"Accelerating $x")
+      Thread.sleep(100)
+      x
+    })
+
+    sourceShape ~> mergeShape ~> repeaterShape
+    mergeShape <~ repeaterShape
+    ClosedShape
+  }
+
+  RunnableGraph.fromGraph(bufferRepeater).run()
+
+  /*
+   cycles risk deadlocking
+   - add bounds to the number of element in the cycle
+
+   boundedness vs liveness
+  */
+
+  /**
+   * Challange: create a fan-in shape
+   *  - two inputs which will be fed with EXACTLY ONE number
+   *  - Output will emit an INFINITE FIBONACCI SEQUENCE based off those 2 numbers
+   *
+   *  Hint: Use ZipWIth and Cycles, MergePreferred
+   * */
+  val fibonacciGenerator = GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
+
+    val zip = builder.add(Zip[BigInt, BigInt])
+    val mergePreferred = builder.add(MergePreferred[(BigInt, BigInt)](1))
+    val fiboLogic = builder.add(Flow[(BigInt, BigInt)].map { pair =>
+      val last = pair._1
+      val previous = pair._2
+
+      Thread.sleep(100)
+      (last + previous, last)
+    })
+
+    val broadcast = builder.add(Broadcast[(BigInt, BigInt)](2))
+    val extractLast = builder.add(Flow[(BigInt, BigInt)].map(_._1))
+
+    zip.out ~> mergePreferred           ~> fiboLogic ~> broadcast ~> extractLast
+               mergePreferred.preferred       <~        broadcast
+
+    UniformFanInShape(extractLast.out, zip.in0, zip.in1)
+  }
+
+  val fiboGraph = RunnableGraph.fromGraph(
+    GraphDSL.create() { implicit builder =>
+      import GraphDSL.Implicits._
+
+      val source1 = builder.add(Source.single[BigInt](1))
+      val source2 = builder.add(Source.single[BigInt](1))
+      val sink = builder.add(Sink.foreach[BigInt](println))
+      val fibo = builder.add(fibonacciGenerator)
+
+      source1 ~> fibo.in(0)
+      source2 ~> fibo.in(1)
+      fibo.out ~> sink
+
+      ClosedShape
+    }
+  )
+
+
 
 }
